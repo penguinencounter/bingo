e699cb17b6cdaa5acbb19ff9d27d2c3f91115b02
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2021-01-01 11:54:16 -0800

    Finish bingo card

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
 import random
 from bingo_card import BingoCard
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape(['html', 'xml']),
+    trim_blocks=True,
+    lstrip_blocks=True
+)
+template = env.get_template('card.jinja')
 
 
 with open('bingo_words.txt') as f:
     words = f.read().split('\n')
 
 
-BingoCard(words).stdout_display()
+card = BingoCard(words)
+render = template.render(card=card.card)
+with open('result.html', 'w') as f:
+    f.write(render)
 
