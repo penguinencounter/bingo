73c84ef6c41d8d24e038628bbe32d3d0688569a3
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2021-01-01 15:26:55 -0800

    >

--- picker.py
+++ picker.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def root():
-    return 'test'
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        formdata = request.form
+        if formdata["set_id"]:
+            print('ok')
+            return redirect(f'/{formdata["set_id"]}/')
+        else:
+            return render_template('index.html')
+
+
+@app.route('/<set_id>/')
+def application(set_id):
+    return set_id
+
 
 app.run(debug=True, host='0.0.0.0')
